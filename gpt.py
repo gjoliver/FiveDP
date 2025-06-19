7e8484e471b376e7910b358beb1cf1e99381641c
Jun Gong <jun@evolutionaryscale.ai>
2025-06-19 10:37:37 -0700

    init

---
+++ gpt.py
@@ -0,0 +1,460 @@
+# pyright: reportGeneralTypeIssues=false, reportAttributeAccessIssue=false, reportCallIssue=false, reportArgumentType=false
+
+import math
+from dataclasses import dataclass
+import os
+
+from datasets import load_dataset
+import torch
+import torch.distributed as dist
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, ShardingStrategy, StateDictType
+from torch.distributed.tensor import Replicate, Shard
+from torch.distributed.tensor.parallel import (
+    ColwiseParallel,
+    parallelize_module,
+    PrepareModuleInput,
+    RowwiseParallel,
+    SequenceParallel,
+)
+import torch.multiprocessing as mp
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, DistributedSampler
+from transformers import GPT2Tokenizer
+
+
+IGNORE_INDEX = -100
+WORLD_SIZE = 8
+BATCH_SIZE = 1
+
+
+@dataclass
+class GPTConfig:
+    # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    vocab_size: int = 50304
+
+    context_length: int = 1024
+
+    n_layer: int = 5
+    n_head: int = 8
+    n_embd: int = 256
+
+    dropout: float = 0.0
+    bias: bool = True
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, ndim: int, bias: bool):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        return F.layer_norm(
+            input, self.weight.shape, self.weight, self.bias, 1e-5,
+        )
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu    = nn.GELU()
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        # embedding size needs to be evenly split to multiple heads.
+        assert config.n_embd % config.n_head == 0
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.d_head = config.n_embd // config.n_head
+        self.dropout = config.dropout
+
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        B, T, D = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch.
+        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+
+        k = k.view(B, T, self.n_head, self.d_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, self.d_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, self.d_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        y = torch.nn.functional.scaled_dot_product_attention(
+            q,
+            k,
+            v,
+            attn_mask=None,
+            dropout_p=self.dropout if self.training else 0,
+            is_causal=True,
+        )
+
+        # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, D)
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+
+        return y
+
+
+class AttnBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        # Residue Attention. LayerNorm before Attention.
+        x = x + self.attn(self.ln_1(x))
+        # Residue MLP. LayerNorm before MLP.
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.context_length is not None
+
+        self.config = config
+
+        # Embedding.
+        self.wte = nn.Embedding(config.vocab_size, config.n_embd)
+        self.wpe = nn.Embedding(config.context_length, config.n_embd)
+        # Transformer blocks.
+        self.drop = nn.Dropout(config.dropout)
+        self.attns = nn.ModuleList([AttnBlock(config) for _ in range(config.n_layer)])
+        self.ln_f = LayerNorm(config.n_embd, bias=config.bias)
+        # Logits.
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # Weight typing.
+        # https://paperswithcode.com/method/weight-tying
+        self.wte.weight = self.lm_head.weight
+
+        # init all weights
+        self.apply(self._init_weights)
+
+        # apply special scaled init to the residual projections, per GPT-2 paper
+        for n, p in self.named_parameters():
+            if n.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, input_ids, is_training: bool = False):
+        device = input_ids.device
+        _, t = input_ids.size()
+        assert t <= self.config.context_length, (
+            f"Cannot forward sequence of length {t}, block size is only {self.config.context_length}"
+        )
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+
+        # forward the GPT model itself
+
+        # token embeddings of shape (b, t, n_embd)
+        tok_emb = self.wte(input_ids)
+        # position embeddings of shape (t, n_embd)
+        pos_emb = self.wpe(pos)
+        # Random input drop out.
+        x = self.drop(tok_emb + pos_emb)
+        # Transformer stack.
+        for block in self.attns:
+            x = block(x)
+        # LayerNorm before logits head.
+        x = self.ln_f(x)
+
+        if is_training:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+        else:
+            # inference-time: only forward the lm_head on the very last position.
+            # note: using list [-1] to preserve the sequence dim.
+            logits = self.lm_head(x[:, [-1], :])
+
+        return logits
+
+    @torch.no_grad()
+    def generate(self, input_ids, max_new_tokens, temperature=1.0, top_k=None):
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at context_length
+            clipped_input_ids = (
+                input_ids
+                if input_ids.size(1) <= self.config.context_length
+                else input_ids[:, -self.config.context_length:]
+            )
+            # forward the model to get the logits for the index in the sequence
+            logits = self(clipped_input_ids)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            input_ids = torch.cat((input_ids, idx_next), dim=1)
+
+        return input_ids
+
+
+def _print_weight(model: FSDP, name: str | None = None):
+    with FSDP.state_dict_type(model, StateDictType.FULL_STATE_DICT):
+        state_dict = model.state_dict()
+
+    if name:
+        print(state_dict[name])
+    else:
+        print(next(iter(state_dict.values())))
+
+
+def _optimizer(gpt: torch.nn.Module):
+    param_dict = {n: p for n, p in gpt.named_parameters() if p.requires_grad}
+
+    # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+    # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+    decay_params = [p for _, p in param_dict.items() if p.dim() >= 2]
+    nodecay_params = [p for _, p in param_dict.items() if p.dim() < 2]
+
+    optim_groups = [
+        {'params': decay_params, 'weight_decay': 1e-1},
+        {'params': nodecay_params, 'weight_decay': 0.0}
+    ]
+    return torch.optim.AdamW(optim_groups, lr=6e-4, betas=(0.9, 0.95))
+
+
+def _dataloader(replicas: int, rank: int):
+    print(f"Creating dataloader, {replicas} replicas, rank {rank}.")
+
+    def _collate(batch):
+        return [
+            row["title"] + " " + row["story"] for row in batch
+        ]
+
+    dataset = load_dataset("FareedKhan/1k_stories_100_genre", split="train")
+    sampler = DistributedSampler(
+        dataset,
+        num_replicas=replicas,
+        rank=rank,
+        shuffle=True,
+    )
+    dataloader = DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=BATCH_SIZE,
+        collate_fn=_collate,
+        shuffle=False,
+    )
+    return dataloader
+
+
+def _init_dist(world_size: int, rank: int):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '8888'
+
+    # Initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def _apply_hsdp(model, device_mesh):
+    return FSDP(
+        model,
+        device_mesh=device_mesh,
+        sharding_strategy=ShardingStrategy.HYBRID_SHARD,  # DDP + FSDP
+        device_id=torch.device("cuda:0"),
+        sync_module_states=True,  # Make sure models on different DDP ranks are synced.
+    )
+
+
+def _apply_sp_tp(model, stp_mesh):
+    # Apply sequence parallel to model level modules.
+    # Namely, we want to shard input embeddings (wte and wpe) ColumnWise
+    # for input into the transformer blocks.
+    # We will then enable SequenceParallel on the last LayerNorm after transformer blocks,
+    # and have the logits head gather all output.
+    parallelize_module(
+        model,
+        stp_mesh,
+        {
+            "wte": RowwiseParallel(
+                input_layouts=Replicate(),
+                output_layouts=Shard(1),  # Desired output is shard on dimension 1 (sequence parallel).
+            ),
+            "wpe": RowwiseParallel(
+                input_layouts=Replicate(),
+                output_layouts=Shard(1),  # Desired output is shard on dimension 1 (sequence parallel).
+            ),
+            # LayerNorm needs to handle sequence parallel because above.
+            "ln_f": SequenceParallel(),
+            # Logits head gathers all output for now.
+            "lm_head": ColwiseParallel(
+                input_layouts=Shard(1),
+                # Not sharding model output for now.
+                # So output layer is Replicate(), while use_local_output is True.
+                output_layouts=Replicate(),
+                use_local_output=True,
+            ),
+        },
+    )
+
+    # Apply tensor + sequence parallelism to every transformer block
+    # NOTE: At the cost of model code change, we can accelerate Sequence Parallel
+    #       by folding (and unfolding) the batch dimension and the sequence dimension.
+    #       Examples can be found at https://github.com/pytorch/torchtitan/pull/437
+    for transformer_block in model.attns.values():
+        layer_plan = {
+            "ln_1": SequenceParallel(),
+            "attn": PrepareModuleInput(
+                input_layouts=(Shard(1), None),  # Because of ln_1 SequenceParallel.
+                desired_input_layouts=(Replicate(), None),  # ATTN itself is TP.
+            ),
+            "attn.c_attn": ColwiseParallel(),  # Columnwise QKV projection.
+            "attn.c_proj": RowwiseParallel(),  # Rowwise FFN projection.
+            "ln_2": SequenceParallel(),
+            "mlp": PrepareModuleInput(
+                input_layouts=(Shard(1),),  # Because of ln_2 SequenceParallel.
+                desired_input_layouts=(Replicate(),),  # MLP itself is TP.
+            ),
+            "mlp.c_fc": ColwiseParallel(),
+            "mlp.gelu": RowwiseParallel(output_layouts=Shard(1)),
+            "mlp.c_proj": ColwiseParallel(),
+        }
+
+        parallelize_module(
+            module=transformer_block,
+            device_mesh=stp_mesh,
+            parallelize_plan=layer_plan,
+        )
+
+
+def train(world_size: int, rank: int):
+    _init_dist(world_size, rank)
+
+    # 2D device mesh on CPU.
+    # Simulate DDP between instances, and FSDP between GPUs on a same instance.
+    device_mesh = dist.init_device_mesh(
+        device_type="cuda", mesh_shape=(2, 2, 2), mesh_dim_names=("ddp", "fsdp", "stp")
+    )
+
+    # Prepare the model.
+    gpt = GPT(GPTConfig())
+    # HSDP: Inter-node DDP + intra-node FSDP.
+    gpt = _apply_hsdp(gpt, device_mesh["ddp", "fsdp"])
+    # SP & TP.
+    gpt = _apply_sp_tp(gpt, device_mesh["stp"])
+
+    # Tokenizer
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Optimizer.
+    optimizer = _optimizer(gpt)
+
+    # Dataloader.
+    dataloader = _dataloader(replicas=device_mesh.size(), rank=device_mesh.get_rank())
+
+    for i, batch in enumerate(dataloader):
+        print(f"step {i}")
+
+        # Input and labels.
+        inputs = tokenizer(batch, padding=True, truncation=True, return_tensors='pt')
+
+        input_ids = inputs["input_ids"]
+        # Mask out paddings.
+        attn_mask = inputs["attention_mask"].to(torch.bool)
+        input_ids[~attn_mask] = IGNORE_INDEX
+
+        # Compute probabilities.
+        logits = gpt.forward(input_ids=input_ids, is_training=True)
+
+        shifted_logits = logits[..., :-1, :].contiguous().to("cuda")
+        shifted_labels = input_ids[..., 1:].contiguous().to("cuda")
+
+        # Cross-entropy loss.
+        loss = F.cross_entropy(
+            shifted_logits.view(-1, shifted_logits.size(-1)),
+            shifted_labels.view(-1),
+            ignore_index=IGNORE_INDEX,
+        )
+
+        # Gradient step.
+        loss.backward()
+        optimizer.step()
+
+        optimizer.zero_grad(set_to_none=True)
+
+        break
+
+    dist.destroy_process_group()
+
+
+def launch():
+    mp.set_start_method('spawn')
+
+    processes = []
+    for i in range(WORLD_SIZE):
+        process = mp.Process(target=train, args=(WORLD_SIZE, i))
+        process.start()
+        processes.append(process)
+
+    for p in processes:
+        p.join()
+
+
+if __name__ == "__main__":
+    launch()
